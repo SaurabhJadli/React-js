ad0f387ed28859ecb5f0ebd74c7970c445510332
SaurabhJadli <saurabhjadli7@protonmail.com>
2024-02-24 23:33:45 +0530

    n-top5

---
+++ netflix-top5/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+    <h1>Saurabh's Netflix Pick</h1>
+    <p>List of 5 Best Series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra curricular</li>
+      <li>My Holo Love</li>
+      <li>My first-2 love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </div>,
+  document.getElementById('root')
+);
